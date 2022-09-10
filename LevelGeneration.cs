1db88b3bd734e180d200c80451e3bb5e83366e81
Wanyea Barbel <61568218+Wanyea@users.noreply.github.com>
2022-09-10 12:43:08 -0400

    Add files via upload

---
+++ LevelGeneration.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public int mapWidthInTiles, mapDepthInTiles, heightAboveBaseTiles;
+
+    [SerializeField]
+    private GameObject tilePrefab;
+
+    [SerializeField]
+    public GameObject spawnPointPrefab;
+
+    [SerializeField]
+    public GameObject endPointPrefab;
+
+    [SerializeField]
+    private GameObject m_tiles;
+    private Vector3 tileCenter;
+    
+    [HideInInspector]
+    public Vector3[] m_tile_positions;
+
+    [HideInInspector]
+    public GameObject spawnEnd;
+
+    [HideInInspector]
+    public GameObject spawnPoint; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void GenerateMap() 
+    {
+        // initialize array holding M tiles positions.
+
+        m_tile_positions = new Vector3[mapWidthInTiles + 1];
+
+        // get tile dimensions from the tile Prefab
+        Vector3 tileSize = tilePrefab.GetComponent<MeshRenderer>().bounds.size;
+        int tileWidth = (int)tileSize.x;
+        int tileDepth = (int)tileSize.z;
+
+        //Helper variables.
+        Vector3 mPosition = Vector3.zero;
+        int mTracker = 0;
+        int mSpawnValue = -1;
+        bool mCanSpawn = true;
+
+        for(int xTileIndex = 0; xTileIndex < mapWidthInTiles; xTileIndex++) {
+
+            //Spawn one M tile for every row.
+            if(mTracker == xTileIndex) { mSpawnValue = Random.Range(0, mapDepthInTiles); }  
+            mTracker++;
+
+            for(int zTileIndex = 0; zTileIndex < mapDepthInTiles; zTileIndex++) {
+                mCanSpawn = true;
+                // calculate the tile position based on the X and Z indices
+                Vector3 tilePosition = new Vector3
+                (
+                    this.gameObject.transform.position.x + xTileIndex * tileWidth,
+                    this.gameObject.transform.position.y,
+                    this.gameObject.transform.position.z + zTileIndex * tileDepth
+                );
+
+                // instantiate a new tile 
+                GameObject tile = Instantiate(tilePrefab, tilePosition, Quaternion.identity) as GameObject;
+
+                // instantiate spawn at random position on 1st tile -- don't allow an M tile to spawn here.
+                if(xTileIndex == 0 && zTileIndex == 0) {
+                    Vector3 spawnPosition = new Vector3
+                    (
+                        Random.Range(-tileWidth / 2, tileWidth / 2),
+                        this.gameObject.transform.position.y + heightAboveBaseTiles,
+                        Random.Range(-tileDepth / 2, tileDepth / 2)
+                    );
+
+                    spawnPoint = Instantiate(spawnPointPrefab, spawnPosition, Quaternion.identity) as GameObject;
+                    spawnPoint.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+                    Debug.Log(spawnPoint.transform.position);
+                    mCanSpawn = false;
+                }
+                
+                // instantiate end at random position on the nth tile -- don't allow M tiles to spawn here.
+                if(xTileIndex == mapWidthInTiles - 1 && zTileIndex == mapDepthInTiles - 1) {
+                    Vector3 endPosition = new Vector3
+                    (
+                      tile.transform.position.x + Random.Range(-tileWidth / 2, tileWidth / 2),
+                      this.gameObject.transform.position.y + heightAboveBaseTiles,
+                      tile.transform.position.z + Random.Range(-tileDepth / 2, tileDepth / 2)  
+                    );
+
+                    spawnEnd = Instantiate(endPointPrefab, endPosition, Quaternion.identity) as GameObject;
+                    spawnEnd.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+                    mCanSpawn = false;
+
+                    //add the final tile at the nth position within the m tiles array.
+                    m_tile_positions[xTileIndex + 1] = endPosition;
+                }
+
+                // Spawn M tile if we are not on first or nth tile, no other tile has been spawned on this row.
+                if(mCanSpawn && mSpawnValue == zTileIndex) 
+                {
+                    mPosition = new Vector3
+                    (
+                      tile.transform.position.x + Random.Range(-tileWidth / 2, tileWidth / 2),
+                      this.gameObject.transform.position.y + heightAboveBaseTiles,
+                      tile.transform.position.z + Random.Range(-tileDepth / 2, tileDepth / 2)  
+                    );
+
+                    GameObject spawnMTiles = Instantiate(m_tiles, mPosition, Quaternion.identity) as GameObject;
+                    spawnMTiles.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+
+                    //Store array of M tile positions to feed into pathfinder. 
+                    m_tile_positions[xTileIndex] = mPosition;
+
+                }
+
+            }
+        }
+    }
+}
---
+++ PathfinderScript.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+[RequireComponent(typeof(PathCreator))]
+public class PathfinderScript : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject pathfinder;
+
+    [SerializeField]
+    float movementSpeed = 10f;
+    [SerializeField]
+    float rotationalDamp = .5f;
+
+    [SerializeField]
+    float rayCastOffset = 1f;
+
+    [SerializeField]
+    float detectionDistance = 20f; 
+
+    [HideInInspector]
+    public int flag = 0;
+    private Vector3 target;
+    private GameObject endTile;
+    private int mTracker = 0;
+    private Vector3[] m_tiles;
+    private GameObject spawnTile;
+    private int numOfM_Tiles;
+
+    [HideInInspector]
+    public Vector3[] pathfinderPosition;
+
+    private int pathfinderPositionIndex = 0;
+    public bool closedLoop = false;
+
+    private GameObject pathy;
+    
+
+    void Start() 
+    {
+        LevelGeneration levelGenerationScript = GetComponent<LevelGeneration>();
+        numOfM_Tiles = levelGenerationScript.mapDepthInTiles;
+        m_tiles = levelGenerationScript.m_tile_positions;
+        spawnTile = levelGenerationScript.spawnPoint;
+        endTile = levelGenerationScript.spawnEnd;
+        pathfinderPosition = new Vector3[1000]; 
+
+        Debug.Log(spawnTile.transform.position);
+        pathy = Instantiate(pathfinder, new Vector3(spawnTile.transform.position.x, spawnTile.transform.position.y + 10, spawnTile.transform.position.z) , Quaternion.identity);
+
+        if(pathfinder != null) 
+            InvokeRepeating("StorePathfinderPosition", 0.0f, 0.3f);
+        
+    }
+
+
+    void Update()
+    {
+        Move();
+        Turn();
+        AvoidCollision();
+
+    }
+
+    void Turn() 
+    {
+        target = m_tiles[mTracker];
+        Vector3 relativePosition = target - pathy.transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePosition);
+        pathy.transform.rotation = Quaternion.Slerp(pathy.transform.rotation, rotation, rotationalDamp * Time.deltaTime);
+        UpdateTarget();
+    }
+
+    void Move() 
+    {
+        pathy.transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+
+    void AvoidCollision() 
+    {
+        RaycastHit hit;
+        Vector3 offset = Vector3.zero;
+
+        Vector3 left = pathy.transform.position - transform.right * rayCastOffset;
+        Vector3 right = pathy.transform.position + transform.right * rayCastOffset;
+        Vector3 up = pathy.transform.position + transform.up * rayCastOffset;
+        Vector3 down = pathy.transform.position - transform.up * rayCastOffset;
+
+        Debug.DrawRay(left, transform.forward * detectionDistance, Color.red);
+        Debug.DrawRay(right, transform.forward * detectionDistance, Color.red);
+        Debug.DrawRay(up, transform.forward * detectionDistance, Color.red);
+        Debug.DrawRay(down, transform.forward * detectionDistance, Color.red);
+
+        //Handle raycast collisions on left and right and move out of way accordingly.
+        if(Physics.Raycast(left, transform.forward, out hit, detectionDistance)) 
+            offset += Vector3.right;    
+        else if(Physics.Raycast(right, transform.forward, out hit, detectionDistance))
+            offset -= Vector3.left;  
+
+        //Handle raycast collisions above and below and move out of way accordingly.
+        if(Physics.Raycast(up, transform.forward, out hit, detectionDistance)) 
+            offset -= Vector3.up;    
+        else if(Physics.Raycast(down, transform.forward, out hit, detectionDistance))
+            offset += Vector3.up;  
+        
+        if(offset != Vector3.zero) {
+            transform.Rotate(offset * 5f * Time.deltaTime);
+        } else {
+            Turn();
+        }
+    }
+
+    void UpdateTarget() 
+    {
+        //Update target for 0 ---> n - 1 tiles. 
+        if(target != endTile.transform.position) {
+            if(Vector3Int.RoundToInt(pathy.transform.position) == m_tiles[mTracker]) 
+                this.mTracker++;
+        }
+
+        //Handle n (final state) case.
+        if(Vector3Int.RoundToInt(pathy.transform.position) == endTile.transform.position) {
+            endTile.GetComponent<Renderer>().material.SetColor("_Color", Color.green);  
+            Destroy(pathfinder);  
+            GenerateBezierPath();    
+        }
+
+    }
+
+    void StorePathfinderPosition() {
+       pathfinderPosition[this.pathfinderPositionIndex] = pathy.transform.position;
+       //Debug.Log(this.pathfinderPosition[pathfinderPositionIndex]);
+       this.pathfinderPositionIndex++;
+    }
+
+    void GenerateBezierPath() {
+          BezierPath bezierPath = new BezierPath(pathfinderPosition, closedLoop,  PathSpace.xyz);
+        }
+}
